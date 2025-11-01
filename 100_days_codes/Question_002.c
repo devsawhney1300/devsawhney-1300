7aa72898bc0f671d353a977b7dadf3a9e3b949fb
devsawhney-1300 <devsawhneyl@gmail.com>
2025-11-01 21:11:22 +0530

    question2

--- 100_days_codes/Question_002.c
+++ 100_days_codes/Question_002.c
@@ -1,21 +1,30 @@
-//Q1: Write a program to input two numbers and display their sum.//
+//Q2 : Write a program to input two numbers and display their sum, difference, product, and quotient.//
 #include<stdio.h>
 
 int main()
 {
     int a,b;
-    printf("Enter two Numbers");
-    scanf("%d %d",&a,&b);
-    printf("The sum = %d",a+b);
+    printf("Enter two Numbers=\n");
+    scanf("%d %d\n",&a,&b);
+    printf("The sum= %d\n",a+b);
+    printf("The difference= %d\n",a-b);
+    printf("The Product = %d\n",a*b);
+    if(b!=0)
+    printf("The Quotient = %d\n",a/b);
+    else
+    printf("Not defined");
     return 0;
 }
-/*Sample Test Cases: 
+
+/*
+Sample Test Cases:
 Input 1:
-3 4
+10 2
 Output 1:
-Sum = 7
+Sum=12, Diff=8, Product=20, Quotient=5
+
 Input 2:
--1 20
+7 3
 Output 2:
-Sum = 19
+Sum=10, Diff=4, Product=21, Quotient=2
 */
